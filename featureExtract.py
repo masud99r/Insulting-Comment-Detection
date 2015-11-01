4c949346662334df97c1fdb46a61f1024681d418
wasiuva <wasicse90@gmail.com>
2015-11-01 14:15:32 -0500

    second commit @1st november, 2015

--- featureExtract.py
+++ featureExtract.py
@@ -1,22 +1,52 @@
 __author__ = 'Wasi Uddin Ahmad, Md Masudur Rahman'
 
+import re
 import string
+import numpy as np
 from nltk import wordpunct_tokenize
+from nltk.tokenize import RegexpTokenizer
 from nltk import bigrams
 from nltk.stem.snowball import EnglishStemmer # loading the stemmer module from NLTK
 
+def removeHTMLTages(comment):
+	p = re.compile(r'<.*?>')
+	comment = p.sub(' ', comment)
+	comment = comment.replace('&nbsp;',' ')
+	return comment
+
+def removeBadTokens(comment):
+	rep = {'\\xa0': ' ', '\\xc2': ' ', '\n': ' ', '\r': ''}
+	rep = dict((re.escape(k), v) for k, v in rep.items())
+	pattern = re.compile("|".join(rep.keys()))
+	comment = pattern.sub(lambda m: rep[re.escape(m.group(0))], comment)
+	return comment
+
 def commentTokenizer(comment):
+	comment = comment.lower()
+	comment = removeHTMLTages(comment)
+	comment = removeBadTokens(comment)
 	tokenList = wordpunct_tokenize(comment)
 	#bi_tokens = [ " ".join(pair) for pair in bigrams(tokenList)]
 	#tokenList = tokenList + bi_tokens
 	return tokenList # returns list of tokens
 
-def commentNormalizer(tokenList):
+def removeStopWords(tokenList):
+	stopwordList = np.genfromtxt('stopwords.txt',dtype='str')
+	filteredWords = [w for w in tokenList if not w in stopwordList]
+	return filteredWords
+
+def commentNormalizer(comment):
+	comment = "".join([ch for ch in comment if ch not in string.punctuation])
+	comment = re.sub("\\d+(\\.\\d+)?", "NUM", comment)
+	return comment
+	'''
 	for i in range(len(tokenList)):
 		tokenList[i] = "".join([ch for ch in tokenList[i] if ch not in string.punctuation])
+		tokenList[i] = re.sub("\\d+(\\.\\d+)?", "NUM", tokenList[i])
 	
 	tokenList = list(filter(None, tokenList)) # filters all empty list
 	return tokenList # returns list of tokens after normalization
+	'''
 
 def commentStemmer(tokenList):
 	stemmer = EnglishStemmer()
--- mainProgram.py
+++ mainProgram.py
@@ -6,6 +6,7 @@ from commentClass import Comment
 
 listOfComments = []
 listOfUniqueTokens = [] # unique tokens of the entire corpus
+documentFrequencyOfTokens = {}
 
 def calculateTermFrequency(tokenList):
 	dictionary = {}
@@ -27,14 +28,21 @@ if __name__ == '__main__':
 	for i in range(len(listOfComments)):
 		content = listOfComments[i].getContent()
 		status = listOfComments[i].getStatus()
+		content = fext.commentNormalizer(content)
 		tokenList = fext.commentTokenizer(content)
-		tokenList = fext.commentNormalizer(tokenList)
+		tokenList = fext.removeStopWords(tokenList)
 		tokenList = fext.commentStemmer(tokenList)
 		listOfUniqueTokens = listOfUniqueTokens + tokenList  # dictionary containing unique tokens
 
 		dicTokens = calculateTermFrequency(tokenList)
 		listOfComments[i].setTokenList(dicTokens)
 
+		for key, value in dicTokens.items():
+			if key in documentFrequencyOfTokens:
+				documentFrequencyOfTokens[key] += 1
+			else:
+				documentFrequencyOfTokens[key] = 1
+		
 		'''
 		print(listOfComments[i].getContent())
 		print('***************************')
@@ -43,4 +51,9 @@ if __name__ == '__main__':
 		'''
 
 	listOfUniqueTokens = list(set(listOfUniqueTokens))
-	print(listOfUniqueTokens)
+	print(len(listOfUniqueTokens))
+	documentFrequencyOfTokens = sorted(documentFrequencyOfTokens.items(), key=lambda x: x[1], reverse=True)
+	for i in range(len(documentFrequencyOfTokens)):
+		var = documentFrequencyOfTokens[i]
+		if var[1] > 5:
+			print(var[0], ' : ', var[1])
